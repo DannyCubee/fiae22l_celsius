30d81051d4b4d8f486a281564d423a624719f57b
Victor <maiholdvictor@gmail.com>
2023-12-20 11:34:36 +0100

    Main.py an neue Crud methoden angepasst

--- app/main.py
+++ app/main.py
@@ -61,7 +61,7 @@ def create_value():
 # noinspection PyTypeChecker
 @app.post("/api/v1/new-temperatures")
 def create_new_temperatures(id: int, temp_c: float, temp_f: float, client: str, db: Session = Depends(get_db)):
-    return crud.create_readings(db, id, temp_c, temp_f, client)
+    return crud.create_reading(db, id, temp_c, temp_f, client)
 
 
 # noinspection PyTypeChecker
@@ -72,42 +72,48 @@ def create_new_temperature(id: int, temp: float, is_celsius: bool, client: str, 
 
 @app.get("/api/v1/all-temperatures", response_model=schemas.Temperature)
 def get_all_temperatures(db: Session = Depends(get_db)):
-    return crud.read_reading(db)
+    return crud.read_all(db)
 
 
 @app.get("/api/v1/sort-id")
 def get_id_in_order(db: Session = Depends(get_db)):
-    return crud.read_reading_Ids(db)
+    return crud.read_Ids(db)
 
+@app.get("/api/v1/last-reading")
+def get_last_id(db: Session = Depends(get_db)):
+    return crud.read_Ids(db, True, limit=1)
 
 @app.get("/api/v1/temperature/{id}", response_model=schemas.Temperature)
 def get_both_temperatures_by_id(id: int, db: Session = Depends(get_db)):
-    return crud.read_reading_by_Id(db, id)
+    return crud.read_by_Id(db, id)
 
 
 @app.get("/api/v1/temerature/{id}")
 def get_temperature_by_id(id: int, is_celsius: bool, db: Session = Depends(get_db)):
-    return crud.read_reading_Temperature(db, id, is_celsius)
+    if bool:
+         return crud.read_Temperature(db, id, "celsius")
+    else:
+        return crud.read_Temperature(db, id, "fahrenheit")
 
 
 @app.get("/api/v1/get-celsius-temps")
 def get_all_celsius_temperatures(db: Session = Depends(get_db)):
-    return crud.read_reading_Celsius(db)
+    return crud.read_Celsius(db)
 
 
 @app.get("/api/v1/get-fahrenheit-temps")
 def get_all_fahrenheit_temperatures(db: Session = Depends(get_db)):
-    return crud.read_reading_Fahrenheit(db)
+    return crud.read_Fahrenheit(db)
 
 
 @app.get("/api/v1/get-temps-from-client")
 def get_all_temperatures_from_client(client: str, db: Session = Depends(get_db)):
-    return crud.read_reading_by_Client(db, client)
+    return crud.read_by_Client(db, client)
 
 
 @app.get("/api/v1/get-temps-from-client-and-time")
 def get_temperature_by_client_and_time(time: datetime, client: str, db: Session = Depends(get_db)):
-    return crud.read_reading_by_Client_and_Time(db, client, time)
+    return crud.read_by_Client_and_Time(db, client, time)
 
 
 @app.put("api/v1/update_temperature_of_id")
@@ -118,7 +124,7 @@ def update_temperature_of_id(temp_c: float, temp_f: float, id: int, db: Session 
 
 @app.delete("/api/v1/delete-temperature")
 def delete_temperature_of_id(id: int, db: Session = Depends(get_db)):
-    return crud.delete_reading(db, id)
+    return crud.delete_by_Id(db, id)
 
 @app.get("/index", response_class=HTMLResponse)
 def launch_index():
